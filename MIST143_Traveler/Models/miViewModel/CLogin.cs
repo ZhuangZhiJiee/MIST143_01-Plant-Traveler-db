5dc53a3ce7cd57bbaba1cb7848a6d300db89c6d3
hiroki311 <miyol311@yahoo.com.tw>
2022-10-12 19:57:23 +0800

    10127

---
+++ MIST143_Traveler/Models/miViewModel/CLogin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MIST143_Traveler.Models.miViewModel
+{
+    public class CLogin
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
