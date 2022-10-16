ac947b1aad0330a8cfdfebd126e9ab584eee39c9
ZhuangZhiJiee <108928269+ZhuangZhiJiee@users.noreply.github.com>
2022-10-16 11:59:16 +0800

    10/15 (#31)
    Co-authored-by: hiroki311 <miyol311@yahoo.com.tw>

---
+++ MIST143_Traveler/Views/CustomerCenter/test.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MIST143_Traveler.Views.CustomerCenter
+{
+    public class testModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
