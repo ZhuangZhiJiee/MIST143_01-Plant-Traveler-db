3c2ea66c640155cc16823e4498b76a5fc1b7b675
ZhuangZhiJiee <108928269+ZhuangZhiJiee@users.noreply.github.com>
2022-10-18 20:30:18 +0800

    1018ZJ (#42)

---
+++ MIST143_Traveler/MViewModel/CProductxViewModel.cs
@@ -0,0 +1,29 @@
+﻿using MIST143_Traveler.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MIST143_Traveler.MViewModel
+{
+    public class CProductxViewModel
+    {
+        public List<產品格式> 產品列表;
+
+    }
+    public class 產品格式
+    {
+        public int TravelProductId { get; set; }
+        public string TravelProductName { get; set; }
+        public decimal Price { get; set; }
+        public int TravelProductTypeId { get; set; }
+        public int Stocks { get; set; }
+        public string Description { get; set; }
+        public int CountryId { get; set; }
+        public int Cost { get; set; }
+        public string EventIntroduction { get; set; }
+        public string MapUrl { get; set; }
+        public string PreparationDescription { get; set; }
+        public List<TravelPicture> productpictures { get; set; }
+    }
+}
