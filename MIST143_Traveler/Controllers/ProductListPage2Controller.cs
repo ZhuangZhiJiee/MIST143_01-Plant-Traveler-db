ec3e01c3bf92e1d73586af6b8e87376731162ccc
emiyalin404 <107528215+emiyalin404@users.noreply.github.com>
2022-10-18 20:31:22 +0800

    yo1018 (#41)
    Co-authored-by: ZhuangZhiJiee <108928269+ZhuangZhiJiee@users.noreply.github.com>

--- MIST143_Traveler/Controllers/ProductListPage2Controller.cs
+++ MIST143_Traveler/Controllers/ProductListPage2Controller.cs
@@ -9,22 +9,50 @@ using System.Threading.Tasks;
 namespace MIST143_Traveler.Controllers
 {
     public class ProductListPage2Controller : Controller
-    {     
+    {
         private readonly PlanetTravelContext _planet;
         public ProductListPage2Controller(PlanetTravelContext planet)
         {
-            _planet=planet;
+            _planet = planet;
         }
         public IActionResult Index()
         {
-         
+
             return View();
         }
-        public IActionResult ProductListPage()
+        public IActionResult ProductListPage(string keyword)
         {
-          var q = from p in _planet.TravelProducts
+            ////  string keyword = Request.Form["txtkeyword"];
+            //  IEnumerable<TravelProduct> datas = null;
+            //  if (string.IsNullOrEmpty(keyword))
+            //  {
+            //      keyword= "ProductListPage";
+            //      return View(keyword);
+            //  }
+            //  else
+            //  {
+            //      datas =( from i in _planet.TravelProducts
+            //              where i.TravelProductId == Convert.ToInt32(keyword) || i.TravelProductName.Contains(keyword) || i.Description.Contains(keyword)
+            //              select i).ToList();
+            var q = from p in _planet.TravelProducts
+                    where p.TravelProductName.Contains(keyword) || p.Description.Contains(keyword)
                     select p;
             return View(q.ToList());
+
+        }
+        public IActionResult filter(string keyword)
+        {
+            var q = from p in _planet.TravelProducts
+                    where p.TravelProductName.Contains(keyword) || p.Description.Contains(keyword)
+                    select p;
+            if (q.Count() <= 0)
+            {
+                q = from p in _planet.TravelProducts
+                    select p;
+            }
+            ViewBag.Count = q.Count();
+            //return View();
+            return ViewComponent("Productlistpagi", q.ToList());
         }
     }
 }
