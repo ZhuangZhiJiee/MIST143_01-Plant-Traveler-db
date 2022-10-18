3e6bf3a56e9134448564ad42bdfaef0d3c2420ee
hiroki311 <miyol311@yahoo.com.tw>
2022-10-18 20:35:13 +0800

    1018

---
+++ MIST143_Traveler/MViewModel/COrderID.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MIST143_Traveler.MViewModel
+{
+    public class COrderID
+    {
+        public int OrderId { get; set; }
+        public int MemberId { get; set; }
+    }
+}
