59365d7f5825c27e0f05b724a7798554600ef04a
ZhuangZhiJiee <108928269+ZhuangZhiJiee@users.noreply.github.com>
2022-10-20 20:49:15 +0800

    Mac0846 (#48)
    Co-authored-by: 禹然 王 <a0987234343@gmail.com>

--- MIST143_Traveler/Controllers/TransportationPageController.cs
+++ MIST143_Traveler/Controllers/TransportationPageController.cs
@@ -1,21 +1,44 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MIST143_Traveler.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MIST143_Traveler.MViewModel;
 
 namespace MIST143_Traveler.Controllers
 {
     public class TransportationPageController : Controller
+        
     {
+        private PlanetTravelContext ptc;
+
+        public TransportationPageController(PlanetTravelContext q)
+        {
+            ptc = q;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var datas = from p in ptc.TravelProducts
+                        where p.Country.CountryName=="台灣"
+                        select p;
+            return View(datas);
         }
         public IActionResult TransportationHomePage()
         {
             return View();
             //1234
+        }
+        [HttpPost]
+        public IActionResult TransportationHomePage(CLocationKeyWordViewModel 參數名稱)
+        {
+            return RedirectToAction("showTransportationHomePage", 參數名稱);
+            //1234
+        }
+        public IActionResult showTransportationHomePage(CLocationKeyWordViewModel 傳入的資料)
+        {
+            return View(傳入的資料);
         }
     }
 }
