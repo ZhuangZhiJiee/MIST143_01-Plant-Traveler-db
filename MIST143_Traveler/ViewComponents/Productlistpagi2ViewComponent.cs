185d9c7c889e8f9624fb1a73387e31cd33f04536
Ricky lin <ricky85320@gmail.com>
2022-10-21 17:46:36 +0800

    yo1021

---
+++ MIST143_Traveler/ViewComponents/Productlistpagi2ViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MIST143_Traveler.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MIST143_Traveler.ViewComponents
+{
+    public class Productlistpagi2ViewComponent : Microsoft.AspNetCore.Mvc.ViewComponent
+    {
+        public readonly PlanetTravelContext _planet;
+        public Productlistpagi2ViewComponent(PlanetTravelContext planet)
+    {
+        _planet = planet;
+    }
+        public async Task<IViewComponentResult> InvokeAsync(List<TravelProduct> pr)
+        {
+
+            return View(pr);
+        }
+    }
+}
