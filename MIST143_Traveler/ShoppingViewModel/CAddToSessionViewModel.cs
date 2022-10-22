c9e5d03ad3a21eacae288f705bcb7a197c2e7062
ZhuangZhiJiee <108928269+ZhuangZhiJiee@users.noreply.github.com>
2022-10-23 00:25:42 +0800

    123 (#56)

---
+++ MIST143_Traveler/ShoppingViewModel/CAddToSessionViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MIST143_Traveler.ShoppingViewModel
+{
+    public class CAddToSessionViewModel
+    {
+        public int TravelProductId { get; set; }
+        public int Count { get; set; }
+    }
+}
