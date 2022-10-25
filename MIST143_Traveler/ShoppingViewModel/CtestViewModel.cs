9a16534f5219e318d5b66c684fbeb8aee3608598
ZhuangZhiJiee <108928269+ZhuangZhiJiee@users.noreply.github.com>
2022-10-25 21:01:39 +0800

    123 (#64)

---
+++ MIST143_Traveler/ShoppingViewModel/CtestViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MIST143_Traveler.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MIST143_Traveler.ShoppingViewModel
+{
+    public class CtestViewModel
+    {
+        //public List<TravelProduct> TravelProductId { get; set; }
+        public List<int> TravelProductId { get; set; }
+    }
+
+}
