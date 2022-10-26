8e97b4fb8746d9b49e9df775bf1f132dd675226c
emiyalin404 <107528215+emiyalin404@users.noreply.github.com>
2022-10-26 21:06:45 +0800

    21312121yo (#71)
    * 00000
    
    * 12131321321
    
    Co-authored-by: ZhuangZhiJiee <108928269+ZhuangZhiJiee@users.noreply.github.com>

---
+++ MIST143_Traveler/MViewModel/Ctestlass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MIST143_Traveler.MViewModel
+{
+    public class Ctestlass
+    {
+        public List<string> productpicture { get; set; }
+
+    }
+}
