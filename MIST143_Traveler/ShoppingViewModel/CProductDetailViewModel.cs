323b8ceb3b79d1c3a59dc3540de7fe2bb67ea662
ZhuangZhiJiee <108928269+ZhuangZhiJiee@users.noreply.github.com>
2022-10-26 19:57:21 +0800

    123 (#69)

---
+++ MIST143_Traveler/ShoppingViewModel/CProductDetailViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MIST143_Traveler.ShoppingViewModel
+{
+    public class CProductDetailViewModel
+    {
+        public List<string> ViewName { get; set; }
+    }
+}
