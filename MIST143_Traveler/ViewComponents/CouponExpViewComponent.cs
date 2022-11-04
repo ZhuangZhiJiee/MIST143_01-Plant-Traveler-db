3fa61da2d7ef9cffeb1a5db5ae816a643093920b
hiroki311 <107532552+hiroki311@users.noreply.github.com>
2022-11-04 09:18:19 +0800

    10101 (#112)

---
+++ MIST143_Traveler/ViewComponents/CouponExpViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MIST143_Traveler.MViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MIST143_Traveler.ViewComponents
+{
+    [Microsoft.AspNetCore.Mvc.ViewComponent]
+    public class CouponExpViewComponent : Microsoft.AspNetCore.Mvc.ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(會員中心檢視優惠券 Coup)
+        {
+            return View(Coup);
+        }
+    }
+}
