4a13c88d4ed1c846641c6b92b2ebfd114a509fe6
yddaDcaM <a0987234343@gmail.com>
2022-11-08 00:21:51 +0800

    Mac1108

---
+++ MIST143_Traveler/Models/ProductCoordinate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace MIST143_Traveler.Models
+{
+    public partial class ProductCoordinate
+    {
+        public int ProductNameCoordinateId { get; set; }
+        public int? TravelProductId { get; set; }
+        public int? CoordinateId { get; set; }
+
+        public virtual Coordinate Coordinate { get; set; }
+        public virtual TravelProduct TravelProduct { get; set; }
+    }
+}
