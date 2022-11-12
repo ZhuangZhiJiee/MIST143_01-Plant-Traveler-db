b2bbb7d27faab80b419417d775ca7fe41bc40ba0
hiroki311 <miyol311@yahoo.com.tw>
2022-11-13 00:26:20 +0800

    111333

--- MIST143_Traveler/MViewModel/CLogin.cs
+++ MIST143_Traveler/MViewModel/CLogin.cs
@@ -10,5 +10,7 @@ namespace MIST143_Traveler.Models.miViewModel
         public string Email { get; set; }
         public string Password { get; set; }
        public string KeepLogin { get; set; }
+
+        public string CAPTCHA { get; set; }
     }
 }
